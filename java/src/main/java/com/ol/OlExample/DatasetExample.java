7922ae5b2a92f6b6a04f000eea1209ee654d09ba
adaverse <2akash111998@gmail.com>
2025-04-30 11:46:01 +0530

    dataset event added

--- java/src/main/java/com/ol/OlExample/DatasetExample.java
+++ java/src/main/java/com/ol/OlExample/DatasetExample.java
@@ -9,9 +9,11 @@ import io.openlineage.client.OpenLineage.JobFacets;
 import io.openlineage.client.OpenLineage.OutputDataset;
 import io.openlineage.client.OpenLineage.Run;
 import io.openlineage.client.OpenLineage.RunFacets;
+import io.openlineage.client.OpenLineage.SchemaDatasetFacet;
 import io.openlineage.client.OpenLineage.RunEvent.EventType;
 import io.openlineage.client.transports.ConsoleTransport;
 import io.openlineage.client.utils.UUIDUtils;
+import io.openlineage.client.OpenLineage.SchemaDatasetFacetFields;
 
 import java.net.URI;
 import java.time.ZoneId;
@@ -26,8 +28,25 @@ public class DatasetExample {
     public static void emitRunEvent(
         String namespace
     ) {
-        OpenLineageClient ol = OlClient.getOlClient();
-
-        ...
+        URI producer = URI.create("producer");
+        OpenLineage ol = new OpenLineage(producer);
+        List<SchemaDatasetFacetFields> fields = Arrays.asList(
+            ol.newSchemaDatasetFacetFields("id", "INTEGER", null, null)
+        );
+        List<InputDataset> inputs =
+        Arrays.asList(
+            ol.newInputDatasetBuilder()
+                .namespace("ins")
+                .name("input")
+                .facets(
+                    ol.newDatasetFacetsBuilder()
+                        .version(ol.newDatasetVersionDatasetFacet("input-version"))
+                        .schema(ol.newSchemaDatasetFacet(Arrays.asList()))
+                        .build())
+                .inputFacets(
+                    ol.newInputDatasetInputFacetsBuilder()
+                        .
+                        .build())
+                .build());
     };
 }
--- java/src/main/java/com/ol/springboot/OlController.java
+++ java/src/main/java/com/ol/springboot/OlController.java
@@ -4,19 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ol.OlClient.CreateNamespace;
-import com.ol.OlClient.OlClient;
-
-import io.openlineage.client.OpenLineageClient;
+import com.ol.OlExample.DatasetExample;
 
 @RestController
 public class OlController {
 
 	@GetMapping("/")
 	public String index() {
-        OpenLineageClient olClient = OlClient.getOlClient();
-        String hi = CreateNamespace.createNamespace("test", "env", "prefix", true);
-        System.out.println(hi);
-		return "Greetings from Spring Boot!";
+        String ns = CreateNamespace.createNamespace("test", "env_java", "prefix", true);
+        DatasetExample.emitRunEvent(ns);
+		return "Sample dataset event emitted";
 	}
 
 }
